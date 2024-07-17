99eb136b00597abab9a10b40214d72a654e39c81
mccedddy <cedricmacallan@gmail.com>
2024-07-18 01:53:35 +0800

    initialized home page and navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex flex-col items-center">
+      <Navbar />
+      <div className="h-4/6 flex flex-col justify-center">
+        <h1 className="">Hi! I'm</h1>
+        <h1 className="text-3xl font-bold">Daniel John Cedric Macallan</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="w-full p-4 border-b-2">
+      <a href="https://www.github.com/mccedddy">GitHub</a>
+    </nav>
+  );
+}
+
+export default Navbar;
