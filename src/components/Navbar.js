82794577b686dbd2564ec5c1a903da91d0aa317e
mccedddy <cedricmacallan@gmail.com>
2024-09-15 14:31:46 +0800

    added icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import DJCMTheme from "../assets/icons/DJCMTheme.svg";
 
 function Navbar() {
   return (
-    <nav className="w-full p-4 flex justify-end absolute" id="home">
-      <div className="flex gap-4">
+    <nav
+      className="w-full p-4 flex justify-between items-center absolute"
+      id="home"
+    >
+      <img src={DJCMTheme} alt="DJCM Logo" className="h-8 w-8" />
+      <div className="flex items-center gap-4">
         <a
           href="#about"
           className="hover:-translate-y-1 transition-transform text-textColor hover:text-customBlue"
