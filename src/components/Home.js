84e22447b16d0bb944a9709315b54bd5f889dd73
mccedddy <cedricmacallan@gmail.com>
2024-09-17 16:18:41 +0800

    fixed home layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,12 @@ function Home() {
 
   return (
     <section className="flex flex-col justify-center items-center p-4 flex-grow">
-      <div>
-        <h1 className="text-textColor text-lg">Hi! I'm</h1>
-        <h1 className="text-4xl font-bold">DANIEL JOHN CEDRIC MACALLAN</h1>
-        <h1 className="text-textColor text-lg">Web Developer</h1>
+      <div className="p-6 lg:p-0 flex flex-col gap-1">
+        <h1 className="text-textColor text-xl">Hi! I'm</h1>
+        <h1 className="text-4xl lg:text-5xl font-bold">
+          DANIEL JOHN CEDRIC MACALLAN
+        </h1>
+        <h1 className="text-textColor text-xl">Web Developer</h1>
 
         <div className="flex gap-3 mt-2">
           {/* Gmail */}
